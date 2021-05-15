7f1bceb271c49e74d7193f5b4df539750cac3e0e
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-05-15 07:33:43 +0200

    Initial Commit

---
+++ InstaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by sanmi_personal on 15/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
